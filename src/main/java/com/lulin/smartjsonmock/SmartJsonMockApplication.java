cbf45d021e8d6575b365858a15238556b8174ecb
lulin525 <2042327049@qq.com>
2021-07-12 18:37:55 +0800

    初始化项目 commit

---
+++ src/main/java/com/lulin/smartjsonmock/SmartJsonMockApplication.java
@@ -0,0 +1,13 @@
+package com.lulin.smartjsonmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartJsonMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartJsonMockApplication.class, args);
+    }
+
+}
